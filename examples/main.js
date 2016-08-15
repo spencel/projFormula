238ab11bdb22a9d8c5a86f3e22968b34fc16fd8d
Spencer Lank <spence.lank@gmail.com>
2016-08-15 07:58:30 -0700

    omg

--- examples/main.js
+++ examples/main.js
@@ -1,15 +1,49 @@
 function main() {
 
+	/*
+	jQuery("body").html("<div id='MathDiv'>The answer you provided is: \\({}\\).</div>");
 
-	var strHtml = MathJax.HTML.Element(
-		"div",
-		{id: "MathDiv"},
-		["\\(F=ma\\)"]
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub,"MathDiv"]) // Might not need this here?
+	
+	jQuery( document ).on( "click", function() {
+
+		
+		
+		var studentDisplay = null;
+		MathJax.Hub.Queue(function () {
+			studentDisplay = MathJax.Hub.getAllJax("MathDiv")[0];
+		});
+
+		MathJax.Hub.Queue(["Text",studentDisplay,"x+1"]);
+
+	});*/
+	
+
+	jQuery( document ).on( "click", function() {
+
+		var strHtml = MathJax.HTML.Element(
+			"div",
+			{id: "MathDiv"},
+			["\\(F=ma\\)"]
+		);
+		
+		jQuery( "body" ).html( strHtml ).css({ "visibility": "hidden" });
+
+		MathJax.Hub.Queue([ "Typeset", MathJax.Hub, "MathDiv" ]);
+
+		var Preview = {
+			makeVisible: function () {
+				jQuery( "body" ).css({ "visibility": "visible" });
+			}
+		}
+
+		MathJax.Hub.Queue(
+			[ "Typeset", MathJax.Hub, "MathDiv" ],
+			[ "makeVisible", Preview ]
 		);
 
-	jQuery( "body" ).html( strHtml ); 
+		
 
-	for (var i = 0; i <100000; i++){
-		console.log(i + " ");
-	}
+	});
+
 }
